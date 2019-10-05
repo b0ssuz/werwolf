16d85074f0559fbecd7bbf6bd0b6571210935c89
b0ssuz <buenyamin.sarikaya@gmail.com>
2019-10-05 11:32:34 +0200

    added description

--- RollenBoese.py
+++ RollenBoese.py
@@ -1,13 +1,16 @@
 from Karte import Karte
 from Boese import Boese
 
+"""
+Rollen: Werwolf, BabyWerwolf
+
+"""
 
 
 class Werwolf(Karte,Boese):
 	def fressen(self,other):
 		if type(other) is BabyWerwolf:
 			other.id = False
-	pass
 class BabyWerwolf(Karte,Boese):
 	id = True # identität auf Gut gesetzt
 	aktiv = False
